cc8efd0944eb09cc75cf0ff821a8ac22e784cc3b
rmeszes <rmeszes1@gmail.com>
2023-05-05 10:02:02 +0200

    tesztesetek

--- String/test.cpp
+++ String/test.cpp
@@ -6,7 +6,18 @@
 #include "gtest_lite.h"
 
 int main() {
-	String s;
-	s.getline(std::cin);
-	std::cout << s;
+	TEST(ctor, string) {
+		String c('c');
+		String s("tor");
+		EXPECT_STREQ("c", c.c_str());
+		EXPECT_STREQ("tor", s.c_str());
+		EXPECT_EQ(1, c.size());
+		EXPECT_EQ(3, s.size());
+	} END
+
+	TEST(copy,string) {
+		String s1("test");
+		String s2 = s1;
+		EXPECT_STREQ(s1.c_str(), s2.c_str());
+	} END
 }
